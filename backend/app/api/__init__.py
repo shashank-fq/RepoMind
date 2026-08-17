bab086229dc1fbf4ad9c22720959a74dbb33bb45
Shashank Shankar <clg.shashank@gmail.com>
2026-08-17 20:41:15 +0530

    updated api/init

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,4 +1,5 @@
 from app.api.repositories import router as repositories_router
 from app.api.files import router as files_router
+from app.api.chunks import router as chunks_router
 
-__all__ = ["repositories_router", "files_router"]
+__all__ = ["repositories_router", "files_router", "chunks_router"]
